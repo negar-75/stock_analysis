4d8f32c8598d476ea663edb2a28a9ae643c73fa2
negar_75 <nasirinegar13@gmail.com>
2026-01-21 11:38:29 +0000

    changed to upper case

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from helpers.data.data_pipeline import DataAnalyze
 from helpers.features.features import FeatureEngineering
 
-raw_path = "/Users/negarnasiri/Documents/python/stock_analysis/data/raw/OCFT.csv"
-clean_path = "/Users/negarnasiri/Documents/python/stock_analysis/data/processed/OCFT_clean.csv"
-types = {
+RAW_PATH = "data/raw/OCFT.csv"
+RAW_PATH = "data/processed/OCFT_clean.csv"
+ANALYTIC_PATH = "data/processed/OCFT_analytic.csv"
+
+DTYPES = {
     "Date": "object",
     "Open": "float64",
     "High": "float64",
@@ -14,7 +16,7 @@ types = {
 }
 
 if __name__ == "__main__":
-    cleaned_data = DataAnalyze(raw_path, types,clean_path)
+    cleaned_data = DataAnalyze(RAW_PATH, DTYPES,RAW_PATH)
     processed_data = cleaned_data.run()
     analytic_data = FeatureEngineering(processed_data, 15, 5)
     df = analytic_data.run()
