c204ef6b341d1069df676e3eb232601dd5485e70
negar_75 <nasirinegar13@gmail.com>
2026-03-05 14:45:10 +0000

    added redis to docker

--- src/stock_analysis/cache/client.py
+++ src/stock_analysis/cache/client.py
@@ -1,3 +1,4 @@
 import redis
+import os
 
-redis_client = redis.Redis.from_url("redis://localhost:6379/0", decode_responses=True)
+redis_client = redis.Redis.from_url(os.getenv("REDIS_URL") or "redis://redis:6379/0", decode_responses=True)
